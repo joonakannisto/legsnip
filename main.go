68b4f825b2ef3dafb05be4c5db28e8c4bd13becc
Joona Kannisto <joona.kannisto@iki.fi>
2017-03-20 12:09:22 +0200

    handlers, how do they work

--- main.go
+++ main.go
@@ -8,21 +8,22 @@ import (
 )
 
 
-http.Handle("/foo", fooHandler)
 
-http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-})
 
 
 func main() {
+
+
+  http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+  	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  })
+
   m := autocert.Manager{
   	Prompt: autocert.AcceptTOS,
   	HostPolicy: autocert.HostWhitelist("porn.kyber.space"),
   }
   s := &http.Server{
   	Addr: ":4430",
-    Handler:        fooHandler,
   	TLSConfig: &tls.Config{GetCertificate: m.GetCertificate},
   }
     err := s.ListenAndServeTLS(":4430", "")
